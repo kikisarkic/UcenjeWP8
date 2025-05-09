2c2d5298126fdad22ad0120e666867e1d20e343d
kikisarkic <Hristinasarkic@gmail.com>
2025-05-09 19:49:32 +0200

    frizerski salon vjezba

---
+++ frizerskisalon.sql
@@ -0,0 +1,25 @@
+﻿--create database frizerskisalon,
+
+--use frizerskisalon;
+
+--create table monalisa(
+--sifra int,
+--naziv varchar(20),
+--ime varchar(20),
+--prezime varchar(20),
+--usluge  varchar (50)
+--);
+
+
+--create table usluge(
+--sifra int,
+--ime varchar(20),
+--prezime varchar(20),
+--djelatnik varchar(20),
+--);
+
+--create table djelatnik(
+--sifra int,
+--korisnik varchar(30),
+--usluga varchar(20)
+--);
