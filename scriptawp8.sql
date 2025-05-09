92e74e719e9a92e06394e64f2e2f37f8aac4bbb0
Mgrga97 <marko.grg97@gmail.com>
2025-05-09 18:06:50 +0200

    kreiranje tablice smjer

--- scriptawp8.sql
+++ scriptawp8.sql
@@ -1,4 +1,15 @@
-﻿--create database edunovawp8
--- drop database edunovawp8;
+﻿
+--create database edunovawp8
 
-use edunovawp8;
+--drop database edunovawp8;
+
+--use edunovawp8;
+-- nazivi tablica u mnozini zbog c# EF
+create table smjerovi (
+sifra int, 
+naziv varchar(50), 
+cjena decimal(18,2),
+datumpokretanja datetime,
+aktivan bit
+);
+
