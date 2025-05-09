24b5854553fc485cfb0290df973b7241e8d23a6d
kikisarkic <Hristinasarkic@gmail.com>
2025-05-09 19:14:53 +0200

    kreiranje baze

--- trgovina.sql
+++ trgovina.sql
@@ -1,1 +1,14 @@
 ﻿
+--create database trgovina;
+--use trgovina;
+
+--create table racuni(
+--sifra int,
+--broj varchar(10),
+--kupac varchar(100)
+--);
+
+create table proizvodi(
+racun varchar(20)
+proizvod varchar(
+);
--- ucenje.sql
+++
@@ -1,9 +0,0 @@
-﻿
---create database trgovina;
---use trgovina;
-
-create table racuni(
-sifra int,
-broj varchar(10),
-kupac varchar(100)
-);
