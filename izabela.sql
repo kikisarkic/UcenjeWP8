c3d4724d3416dd99c7c633f94e32988b4483e964
Kiki <Hristinasarkic@gmail.com>
2025-05-10 19:53:24 +0200

    izabela skola

--- izabela.sql
+++ izabela.sql
@@ -2,10 +2,21 @@
 
 --use izabela;
 
-create table izbela(
-sifra int,
-naziv varchar(20),
-ime varchar(20),
-prezime varchar(20)
+--create table izbela(
+--sifra int,
+--naziv varchar(20),
+--ime varchar(20),
+--prezime varchar(20)
+--);
 
-);
+--create table racuni(
+--sifra int,
+--revolut decimal (3,2),
+--zagrebacka decimal(3,2),
+--erste decimal (3,2)
+--);
+
+--create table skola(
+--sifra int,     
+--prosjek decimal(3,2)
+--);
