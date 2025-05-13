4b30dd1d680a56de6217138f14bdf5744a647f06
kikisarkic <Hristinasarkic@gmail.com>
2025-05-13 17:26:33 +0200

    salonnamjestaja

---
+++ SQLQuery1.sql
@@ -0,0 +1,14 @@
+--create database salonnamjestaja;
+--use salonnamjestaja;
+
+--create table ormari(
+--sifra int (2),
+--nalog varchar (20),
+--tip varchar(20)
+--);
+
+--create table police(
+--sifra int (2),
+--nalog varchar (20),
+--boja varchar(20)
+--);
