050f6befbf4e1a597615e6df6e3bcc70c4d3318d
kikisarkic <Hristinasarkic@gmail.com>
2025-05-16 18:21:53 +0200

    polaznici

--- insert sql.sql
+++ insert sql.sql
@@ -23,9 +23,42 @@ select * from grupe;
 
 insert into grupe (naziv, smjer)
 values 
+
 --1
 ('WP8',1), 
 --2
 ('WP7',1),
 --3
-('S1', 2)
+('S1', 2);
+
+insert into polaznici (prezime,ime,email) values
+
+('Gavran','Barbara','barbaragavran67@gmail.com'),
+('Šteko','Ivan','ivan.steko5@gmail.com'),
+('Jaklić','Bruno','bruno.jaklic.2005@gmail.com'),
+('Sarkić','Hristina','Hristinasarkic@gmail.com'),
+('Petrač','Darijan','darijan.petrac@gmail.com'),
+('Abramić','Barbara','abramic7barbara7@gmail.com'),
+('Dujak','Dražen','dujak.drazen@gmail.com'),
+('Arambašić','Dario','DarioArambasic@outlook.com'),
+('Vučić','Zlata','zltvvu7@gmail.com'),
+('Hadžić','Ivan','ivan.hadzic07@gmail.com'),
+('Živković','Katarina','kzivkovic230@gmail.com'),
+('Klajić','Nataša','nateabach@gmx.net'),
+('Matijević','David','matijevicdavid4@gmail.com'),
+('Nemet Borić','Biljana','biljananemetos@gmail.com'),
+('Somborac','Lara','larasomborac5@gmail.com'),
+('Smrček','Tomislav','tomislavsmrk2@gmail.com'),
+('Hepp','Barbara','barbara-hepp@hotmail.com'),
+('Knežević','Ante','knezevicantisa@gmail.com'),
+('Stošić','Zlatko','stosic.zlatko@gmail.com'),
+('Darojković','Elizabeta','darojkovicelizabeta@gmail.com'),
+('Dadić','Mateja','mateja.dadic@gmail.com'),
+('Božić','Matej','matejbozic57@gmail.com'),
+('Knežević','Drago','knezevicdrago3@gmail.com'),
+('Milić','Lana','lana.milic15@gmail.com'),
+('Ninčević','Domagoj','nincevicdomagoj@gmail.com'),
+('Bašić','Asmir','asmir.basic@zeljko.hr'),
+('Mandić','Andrea','andrea.mandic.anela@gmail.com'),
+('Mirković','Milivoje','daky696@gmail.com'),
+('Andraković','Nenad','nenad.andrak@gmail.com');
