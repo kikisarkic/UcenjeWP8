bfecd844038a3e58f08f3282041e4f8f58388a6d
Kiki <Hristinasarkic@gmail.com>
2025-05-16 08:27:29 +0200

    vjezba knjiznica 15.05

--- knjiznica vijezba.sql
+++ knjiznica vijezba.sql
@@ -1,4 +1,4 @@
---create database knjiznica;
+--create database knjiznica1;
 --use master;
 --go
 --drop database if exists knjiznica;
@@ -7,33 +7,30 @@
 --go
 --use knjiznica;
 
-create table autor(
-sifra int not null primary key identity (1,1),
-ime varchar (50)not null,
-prezime varchar (50) not null,
-datumrodenja datetime not null,
-);
+--create table autor(
+--sifra int not null primary key identity (1,1),
+--ime varchar (50)not null,
+--prezime varchar (50) not null,
+--datumrodenja datetime not null,
+--);
 
-create table izdavac(
-sifra int not null primary key identity (1,1),
-naziv varchar (50) not null,
-aktivan bit not null default 0
-);
+--create table izdavac(
+--sifra int not null primary key identity (1,1),
+--naziv varchar (50) not null,
+--aktivan bit not null default 0
+--);
 
-create table mjesto(
-sifra int not null primary key identity (1,1),
-naziv varchar (50) not null,
-postanskibroj varchar (20) not null,
-drzava varchar (50) not null,
-);
+--create table mjesto(
+--sifra int not null primary key identity (1,1),
+--naziv varchar (50) not null,
+--postanskibroj varchar (20) not null,
+--drzava varchar (50) not null,
+--);
 
-create table katalog(
-sifra int not null primary key identity (1,1),
-autor int not null,
-naslov varchar (50) not null,
-izdavac int not null,
-mjesto int not null
-);
-
-
-
+--create table katalog(
+--sifra int not null primary key identity (1,1),
+--autor int not null,
+--naslov varchar (50) not null,
+--izdavac int not null, 
+--mjesto int not null 
+--);
