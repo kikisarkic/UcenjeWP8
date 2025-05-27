e4834bc6275a8e8f291be0c5a98623089dab6b03
Kiki <Hristinasarkic@gmail.com>
2025-05-27 18:44:11 +0200

    scripta golddigger

--- golddigger.sql
+++ golddigger.sql
@@ -1,40 +1,45 @@
-create database golddigger;
-use golddigger;
+--use master;
+--go
+--drop database if exists golddigger;
+--go 
+--create database golddigger collate Croatian_CI_AS;
+--go
+--use golddigger;
 
-create table radninalog(
-sifra radninalog int,
-datum pocetka datetime,
-datum zavrsetka datetime,
-potrazitelj varchar (50),
-radnik varchar (20),
-stroj varchar (20),
-racun varchar (20)
-);
 
-create table potrazitelj(
-sifra potrazitelj int,
-naziv varchar (50),
-adresa varchar (50),
-OIB varchar (20),
-telefon varchar (20),
-email varchar (20)
-);
+--create table potrazitelji(
+--sifra int not null primary key identity (1,1),
+--naziv varchar (50)not null,
+--adresa varchar (50) not null,
+--OIB varchar (20)not null,
+--telefon varchar (20)not null,
+--email varchar (20)
+--);
 
-create table radnik(
-sifra radnik int,
-ime varchar (20),
-prezime varchar (20),
-telefon varchar
-);
+--create table radnici(
+--sifra int not null primary key identity(1,1),
+--ime varchar (20)not null,
+--prezime varchar (20)not null,
+--telefon varchar (20)
+--);
 
-create table stroj(
-sifra stroja int,
-model varchar (20),
-tip varchar (20)
-);
+--create table strojevi(
+--sifra int not null primary key identity (1,1),
+--model varchar (20),
+--tip varchar (20)
+--);
 
-create table racun(
-sifra racun int,
-iznos varchar (10),
-nacin placanja varchar
-);
+--create table racuni(
+--sifra int not null primary key identity(1,1),
+--iznos decimal (5,2),
+--nacinplacanja varchar (20)
+--);
+
+--create table radninalozi(
+--sifra int not null primary key identity (1,1),
+--datum datetime,
+--potrazitelj int not null references potrazitelji (sifra),
+--radnik int not null references radnici (sifra),
+--stroj int not null references strojevi (sifra),
+--racun int not null references racuni (sifra)
+--);
