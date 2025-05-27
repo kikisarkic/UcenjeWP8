e4da8203db723ba606be0c04e3bc57a20d27fcc5
Kiki <Hristinasarkic@gmail.com>
2025-05-27 17:25:06 +0200

    GOLDDIGGER scripta

--- golddigger.sql
+++ golddigger.sql
@@ -1,21 +1,40 @@
 create database golddigger;
 use golddigger;
 
-create table nalog(
-sifra nalog int,
+create table radninalog(
+sifra radninalog int,
 datum pocetka datetime,
 datum zavrsetka datetime,
-korisnik varchar (50),
+potrazitelj varchar (50),
 radnik varchar (20),
 stroj varchar (20),
 racun varchar (20)
 );
 
-create table korisnik(
-sifra korisnik int,
+create table potrazitelj(
+sifra potrazitelj int,
 naziv varchar (50),
 adresa varchar (50),
 OIB varchar (20),
 telefon varchar (20),
 email varchar (20)
+);
+
+create table radnik(
+sifra radnik int,
+ime varchar (20),
+prezime varchar (20),
+telefon varchar
+);
+
+create table stroj(
+sifra stroja int,
+model varchar (20),
+tip varchar (20)
+);
+
+create table racun(
+sifra racun int,
+iznos varchar (10),
+nacin placanja varchar
 );
