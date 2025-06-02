deb0bed66c6398ca761372d20c2e2434cd364737
Kiki <Hristinasarkic@gmail.com>
2025-06-02 07:13:43 +0200

    trgovina vijezba 30.05.2025.

--- trgovina.sql
+++ trgovina.sql
@@ -1,4 +1,5 @@
-﻿use master;
+﻿
+use master;
 go
 drop database if exists trgovina;
 go
@@ -27,3 +28,29 @@ proizvod int not null references proizvodi (sifra),
 kolicina decimal(5,3) not null
 
 );
+
+--VIJEZBE SELECT, WHERE, FROM, TO, SET, BETWEEN, LIKE, AND,//
+
+use svastara;
+--select getdate (); // datum i vrijeme//
+
+--select * from artikli order by cijena asc; // sve su cijene prikazane po asc//
+select * from artikli where cijena is not null and cijena < 50.00;
+
+
+--//select artikli by cijena and vrsta//
+select * from artikli where cijena between 20 and 100 and kratkinaziv like '%glodalo%';
+
+--//selekt artikli top 20 po kratki naziv ascending order//
+select top 20 * from artikli order by kratkinaziv asc;
+
+select count (*)from artikli;
+select count (*) from mjesta;
+
+--select * from mjesta;
+--select * from mjesta  where opcina = 6149;
+--select top 10*from mjesta;
+select * from mjesta  where naziv like '%tenje%';
+select * from artikli where cijena is not null;
+update artikli set cijena = cijena / 7.5345;
+update artikli set cijena = cijena * 10;
