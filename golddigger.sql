55d476aee0f2b87d1ab225de0c718984e426d97c
Kiki <Hristinasarkic@gmail.com>
2025-06-02 07:15:48 +0200

    scripta Golddigger

--- golddigger.sql
+++ golddigger.sql
@@ -1,10 +1,10 @@
---use master;
---go
---drop database if exists golddigger;
---go 
---create database golddigger collate Croatian_CI_AS;
---go
---use golddigger;
+﻿----use master;
+----go
+----drop database if exists golddigger;
+----go 
+----create database golddigger collate Croatian_CI_AS;
+----go
+----use golddigger;
 
 
 --create table potrazitelji(
@@ -43,3 +43,46 @@
 --stroj int not null references strojevi (sifra),
 --racun int not null references racuni (sifra)
 --);
+
+
+insert into potrazitelji(naziv,adresa,OIB,telefon,email),
+values
+
+(
+);
+
+insert into radnici(ime,prezime,telefon),
+values
+('Bojan Sojic', 0957630848),
+('Sasa Sarkic', 0918800421),
+('Sasa K
+
+);
+
+--insert into strojevi (model,Tip),
+--values
+--(bager, JCB),
+--(bager, Kubota),
+--(kamion, Iveco),
+--(kamion, Man),
+--(kamion, Tam),
+--(Traktor IMT),
+--);
+
+insert into racuni (iznos, nacin placanja),
+values
+);
+
+select * from radnici,
+
+insert into radnici (ime,prezime,telefon),
+values ('Robert', 'Simic', 0913658754)
+);
+
+use golddigger;
+select a.ime,a.prezime, a.telefon
+from radnici a inner join radninalozi b on a.sifra = b.radnik
+
+
+
+
