e9474df2d47971a799886e0133d938ab2a36d9c4
Kiki <Hristinasarkic@gmail.com>
2025-06-03 19:52:44 +0200

    Ulaz Izlaz

---
+++ BACKEND/Ucenje/E01UlazIzlaz.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    internal class E01UlazIzlaz
+
+    {
+//unutar klase radomo metodu
+public static void Izvedi()
+
+        {
+            Console.Write("Pozdrav svijetu");
+
+            Console.WriteLine("Isti red");
+
+            Console.WriteLine("Osijek je \n SUPER"); // \n je novi red");
+
+            Console.WriteLine("\t\t Ovo je uvuceno");
+
+            Console.Write("Unesi svoje ime:");
+
+            // varijabla ime
+            string ime = Console.ReadLine();
+            Console.WriteLine("Unijeli ste {0}", ime);
+
+
+
+//kraj metode
+        }
+
+
+
+    }//kraj klase
+}//kraj imenskog prostora ( namespace)
--- BACKEND/Ucenje/Program.cs
+++ BACKEND/Ucenje/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Ucenje;
+E01UlazIzlaz.Izvedi();
+
+
+
+
+
+
+
+
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
 
