34ba9d99bdf577192f8f867332fcf22b46e8e42c
Kiki <Hristinasarkic@gmail.com>
2025-06-12 13:34:45 +0200

    BackendPetlja update

--- BACKEND/Ucenje/Program.cs
+++ BACKEND/Ucenje/Program.cs
@@ -11,6 +11,15 @@ class Program
         Console.WriteLine("3. Operatori");
         Console.WriteLine("4. Uvjetno Grananje (If)");
         Console.WriteLine("5. Uvjetno Grananje (Switch)");
+
+        Console.WriteLine("6a. Zad1 (Switch)");
+
+        Console.WriteLine("6. Nizovi ");
+        Console.WriteLine("7. ForPetlja ");
+        Console.WriteLine("8. WhilePetlja ");
+        Console.WriteLine("9. DoWhile ");
+        Console.WriteLine("10. ForEach ");
+
         Console.Write("Choice: ");
 
         var input = Console.ReadLine();
@@ -30,6 +39,26 @@ class Program
                 E04UvjetnoGrananjeIf.Izvedi();
                 break;
             case "5":
+
+                E06Zad1.Izvedi();
+                break;
+            case "6a":
+                E06Nizovi.Izvedi();
+                break;
+            case "6":
+                E07ForPetlja.Izvedi();
+                break;
+            case "7":
+               E08WhilePetlja.Izvedi();
+                break;
+            case "8":
+                E09DoWhile.Izvedi();
+                break;
+            case "9":
+                E10ForEach.Izvedi();
+                break;
+            case "10":
+
                 E05UvjetnoGrananjeSwitch.Izvedi();
                 break;
             default:
