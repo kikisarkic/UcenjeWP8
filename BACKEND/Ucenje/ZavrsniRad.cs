d65f907223838e63e557918deba9267c4cb960ae
Kiki <Hristinasarkic@gmail.com>
2025-06-17 19:50:28 +0200

    Zavrsni Rad

---
+++ BACKEND/Ucenje/ZavrsniRad.cs
@@ -0,0 +1,18 @@
+﻿namespace ZavrsniRad
+{
+    public class RadniNalozi
+    {
+        public int sifra { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Potrazitelj { get; set; } = new Potrazitelj;
+        public string Radnik { get; set; } = new Radnik;
+
+        public string Stroj { get; set; } = Stroj;
+        public decimal Racun { get; set; } = Racun;
+
+
+
+
+
+    }
+}
